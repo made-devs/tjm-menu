9e7e9dd49dfb5d18711616cf9a9752c1db060645
madedev <nmaderama@gmail.com>
2025-12-17 16:21:32 +0700

    Unoptimized image

--- next.config.mjs
+++ next.config.mjs
@@ -3,26 +3,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
       },
       {
         // Tambahkan konfigurasi untuk imgur.com di sini
-        protocol: "https",
-        hostname: "imgur.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'imgur.com',
+        port: '',
+        pathname: '/**',
       },
       {
         // Konfigurasi untuk i.imgur.com karena kadang linknya beda
-        protocol: "https",
-        hostname: "i.imgur.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
